81e0c3f4b35376cb0cbdbc359f7306c4bf2b70bf
KarlaCarreto <a01652799@itesm.mx>
2019-05-16 14:34:23 -0500

    Ajuste Menus

--- Assets/Scripts/OpenMenu.cs
+++ Assets/Scripts/OpenMenu.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OpenMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Button quitButton;
+    public GameObject panel;
+
     void Start()
     {
-        
+        quitButton.onClick.AddListener(TaskOnClick);
     }
 
-    // Update is called once per frame
-    void Update()
+    void TaskOnClick()
     {
-        
+        Debug.Log("Quit");
+        panel.gameObject.SetActive(true);
     }
+
 }
