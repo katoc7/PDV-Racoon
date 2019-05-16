02c2bf51d126e2dd98ebaea1d069697e0a825140
KarlaCarreto <a01652799@itesm.mx>
2019-05-16 13:47:58 -0500

    Guardar y cargar - en proceso

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+[System.Serializable]
+
+public class Game
+{
+    public static Game current;
+    public Character racoon;
+
+    public Game()
+    {
+        racoon = new Character();
+    }
+}
