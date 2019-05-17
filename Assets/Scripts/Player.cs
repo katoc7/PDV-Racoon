84a1bf9940784e5657575c94118b34e0390d889d
KarlaCarreto <a01652799@itesm.mx>
2019-05-16 23:05:43 -0500

    Save-Load
    Prueba Funcional

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System;
+
+[System.Serializable]
+
+public class Player
+
+{
+    private int _paints;
+    private int _health;
+    private float _xPos;
+    private float _yPos;
+
+    public Player()
+    {
+
+    }
+
+    public Player(int paints, int health, float xPos, float yPos)
+    {
+        _paints = paints;
+        _health = health;
+        _xPos = xPos;
+        _yPos = yPos;
+    }
+
+    public int Paints
+    {
+        get
+        {
+            return _paints;
+        }
+        set
+        {
+            _paints = value;
+        }
+    }
+
+    public int Health
+    {
+        get
+        {
+            return _health;
+        }
+        set
+        {
+            _paints = value;
+        }
+    }
+
+    public float XPos
+    {
+        get
+        {
+            return _xPos;
+        }
+        set
+        {
+            _xPos = value;
+        }
+    }
+
+    public float YPos
+    {
+        get
+        {
+            return _yPos;
+        }
+        set
+        {
+            _yPos = value;
+        }
+    }
+}
---
+++ Assets/Scripts/SavingData.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class SavingData : MonoBehaviour
+{
+    
+    private string DATA_PATH = "/MyGame.dat";
+    private Player raccon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // SaveData();
+        //print("DATA PHAT IS" + Application.persistentDataPath + DATA_PATH);
+        LoadData();
+        if(raccon != null)
+        {
+            print("Paints" + raccon.Paints);
+            print("Health" + raccon.Health);
+            print("xPos" + raccon.XPos);
+            print("yPos" + raccon.YPos);
+        }
+    }
+
+    // Update is called once per frame
+    void SaveData()
+    {
+        FileStream file = null;
+
+        try
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+
+            file = File.Create(Application.persistentDataPath + DATA_PATH);
+            Player p = new Player(3,60,50,50);
+
+            bf.Serialize(file, p); //encript and saving
+
+        }catch(Exception e)
+        {
+            if (e != null)
+            {
+
+            }
+        }
+        finally
+        {
+            if(file != null)
+            {
+                file.Close();
+            }
+        }
+    }
+
+    void LoadData()
+    {
+        FileStream file = null;
+
+        try
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+
+            file = File.Open(Application.persistentDataPath + DATA_PATH, FileMode.Open);
+            raccon = bf.Deserialize(file) as Player;
+            //decrepting and loading
+
+        }catch(Exception e)
+        {
+
+        }
+        finally
+        {
+            if (file != null)
+            {
+                file.Close();
+            }
+        }
+
+    }
+}
